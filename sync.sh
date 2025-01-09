27e3fd32a14a1163475d75b81b53e76fbdb229cf
DIKIY1111325 <libre.dude@gmail.com>
2025-01-09 13:49:29 +0300

    Автоматическая синхронизация изменений

---
+++ sync.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd "J:/exchange_for_VM/NOTE/продажи сервер/share_for_HTML/2136_kool_form_pack (1)/internet"
+git add .
+git commit -m "Автоматическая синхронизация изменений"
+git push origin master
